eef84f8f356fdfae2997d2e8de44823b8beb0cc1
jendhordejan <jendhordejan@gmail.com>
2020-01-20 13:16:00 +0100

    test react reployment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello! you beatiful creature you!</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello! you beatiful creature you!
+        </a>
+      </header>
     </div>
   );
 }
